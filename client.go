4037c316125d981eb206816f72837118a02d9a72
Sam Vilain <sam@vilain.net>
2017-03-06 14:42:11 -0800

    feature: non-GET requests

--- client.go
+++ client.go
@@ -39,7 +39,10 @@ type SSEClient struct {
 	sync.Mutex
 
 	// info of the request.  The SSE spec only allows GET requests and no body!
-	url *url.URL
+	url     *url.URL
+	verb    string
+	body    io.ReadSeeker
+	headers http.Header
 
 	// current state of the client
 	readyState ReadyState
@@ -115,21 +118,43 @@ func (ssec *SSEClient) SetContext(ctx context.Context) {
 }
 
 // GetStream makes a GET request and returns a channel for *all* events read
-func (ssec *SSEClient) GetStream(uri string) error {
+func (ssec *SSEClient) GetStream(uri string) (<-chan Event, error) {
+	if err := ssec.SetStreamRequest("GET", uri, nil, nil); err != nil {
+		return nil, err
+	}
+	return ssec.stream()
+}
+
+// PostStream makes a POST request and returns a channel for *all*
+// events read.  Note: it is not possible to do this in browsers
+// without special re-implementations of EventSource and IE polyfill
+func (ssec *SSEClient) PostStream(uri string, body io.ReadSeeker, headers http.Header) (<-chan Event, error) {
+	if err := ssec.SetStreamRequest("POST", uri, body, headers); err != nil {
+		return nil, err
+	}
+	return ssec.stream()
+}
+
+// SetStreamRequest allows the request for the next reconnection to be
+// altered
+func (ssec *SSEClient) SetStreamRequest(method, uri string, body io.ReadSeeker, headers http.Header) error {
 	ssec.Lock()
 	defer ssec.Unlock()
+	ssec.verb = method
+	ssec.body = body
+	ssec.headers = headers
 	var err error
-	if ssec.url, err = url.Parse(uri); err != nil {
-		return errors.Wrap(err, "error parsing URL")
+	ssec.url, err = url.Parse(uri)
+	if err == nil && ssec.baseURL != nil {
+		ssec.url = ssec.baseURL.ResolveReference(ssec.url)
 	}
-	go ssec.notify()
 	return err
 }
 
 func (ssec *SSEClient) makeRequest() (*http.Request, error) {
 	ssec.Lock()
 	defer ssec.Unlock()
-	request, err := http.NewRequest("GET", ssec.uri, nil)
+	request, err := http.NewRequest(ssec.verb, ssec.uri, ssec.body)
 	if err != nil {
 		return nil, err
 	}
@@ -139,11 +164,15 @@ func (ssec *SSEClient) makeRequest() (*http.Request, error) {
 	cancelCtx, ssec.cancel = context.WithCancel(context.Background())
 	request := req.WithContext(cancelCtx)
 
+	for header, vals := range ssec.headers {
+		request.Header[header] = vals
+	}
+	return nil
 	request.Header.Set("Accept", "text/event-stream")
 	if ssec.lastEventId != "" {
 		request.Header.Set("Last-Event-Id", ssec.lastEventId)
 	}
-	return req, nil
+	return request, nil
 }
 
 func (ssec *SSEClient) connect() error {
