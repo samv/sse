f15cee72c7cf706d499a19f3c2d088708b06c477
Sam Vilain <sam@vilain.net>
2017-03-06 15:08:50 -0800

    client: core-as-exceptional-api message routing

--- client_test.go
+++ client_test.go
@@ -3,7 +3,11 @@ package sse
 // test scenarios:
 
 //  "happy" case - connect to a server
+//    * client reading just messages
+//    * client watching errors
+//    * client watching for "open"
 
 //  "reconnect" cases -
 //    * reconnects by default
 //    * with a negotiated retry
+//    * client watching for "closed"
